f716467b3789f8e30f65e1a2289ca8b23b305658
Alexander Cyon <alex.cyon@gmail.com>
2021-07-14 21:31:16 +0200

    Successfully rendering an SDL window with a red background using SDL2.

--- Sources/SuccessionsKrigen/main.swift
+++ Sources/SuccessionsKrigen/main.swift
@@ -1,1 +1,33 @@
-print("Hello, world!")
+
+import Foundation
+import CSDL2
+import SwiftSDL2
+
+try SDL.Run { engine in
+    // Start engine ------------------------------------------------------------
+    try engine.start(subsystems: .video)
+    
+    // Create renderer ---------------------------------------------------------
+    let (window, renderer) = try engine.addWindow(width: 640, height: 480)
+    
+    // Handle input ------------------------------------------------------------
+    engine.handleInput = { [weak engine] in
+        var event = SDL_Event()
+        while(SDL_PollEvent(&event) != 0) {
+            if event.type == SDL_QUIT.rawValue {
+                engine?.removeWindow(window)
+                engine?.stop()
+            }
+        }
+    }
+    
+    // Render ------------------------------------------------------------------
+    engine.render = {
+        renderer.result(of: SDL_SetRenderDrawColor, 255, 0, 0, 255)
+        renderer.result(of: SDL_RenderClear)
+        
+        /* Draw your stuff */
+        
+        renderer.pass(to: SDL_RenderPresent)
+    }
+}
