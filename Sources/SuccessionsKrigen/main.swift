697f076f8abfd2a2a6e761cdded31a5cdef38251
Alexander Cyon <alex.cyon@gmail.com>
2021-07-26 15:09:31 +0200

    Display PNG image clean up

--- Sources/SuccessionsKrigen/main.swift
+++ Sources/SuccessionsKrigen/main.swift
@@ -1,136 +1,87 @@
 import Foundation
 
-
-//print("HEJ ALEX, trying to draw main menu...")
-//try! drawMainMenu()
-
-
-import Foundation
-import CSDL2
-import SwiftSDL2
-//
-//func runSDL2Example() throws {
-//
-//    let width: Int32 = 640
-//    let height: Int32 = 480
-//    try SDL.Run { engine in
-//        // Start engine ------------------------------------------------------------
-//        try engine.start(subsystems: .video)
-//
-//        // Create renderer ---------------------------------------------------------
-//        let (window, renderer) = try engine.addWindow(width: width, height: height)
-//
-//        // Handle input ------------------------------------------------------------
-//        engine.handleInput = { [weak engine] in
-//            var event = SDL_Event()
-//            while(SDL_PollEvent(&event) != 0) {
-//                if event.type == SDL_QUIT.rawValue {
-//                    engine?.removeWindow(window)
-//                    engine?.stop()
-//                }
-//            }
-//        }
-//
-//        // Render ------------------------------------------------------------------
-//        engine.render = {
-//            renderer.result(of: SDL_SetRenderDrawColor, 255, 0, 0, 255)
-//            renderer.result(of: SDL_RenderClear)
-//
-//            let depth: Int32 = 32
-//            let rgbFormat: UInt32 = SDL_PIXELFORMAT_RGBA8888.rawValue
-//            var flags: UInt32 = 0
-////            renderer.result(of: SDL_CreateRGBSurfaceWithFormat, flags, width, height, depth, rgbFormat)
-////            renderer.result(of: )
-////            renderer.pass(to: SDL_CreateRGBSurfaceWithFormat, flags, width, height, depth, rgbFormat)
-//            renderer.
-//            /* Draw your stuff */
-//
-//            renderer.pass(to: SDL_RenderPresent)
-//        }
-//    }
-//
-//}
-//
-//try! runSDL2Example()
-
 import HoMM2Engine
+
+import CSDL2
 import CSDL2_Image
 
-
-func testSDL() {
-    let heroes2AggFile = AGGFile.heroes2
-    //            let backgroundImageDataFull = heroes2AggFile.dataFor(icon: .mainMenuBackground)
-    //        let backgroundImageData = Data(backgroundImageDataFull.suffix(from: 6))
-    let hommSpellBMPFull = try! heroes2AggFile.read(fileName: "SPELBW04.BMP")
-    let hommSpellBMP = Data(hommSpellBMPFull.suffix(from: 6))
-    
-    
-    
-    /* Starting SDL */
-    func panic(reason: String, _ line: UInt = #line) -> Never {
+private func sdlError(prefix: String) -> String {
         let maxLen: Int32 = 1000
         var errMessage = Array<CChar>.init(repeating: 0, count: Int(maxLen))
         SDL_GetErrorMsg(&errMessage, maxLen)
         let errorMessage = String(bytes: errMessage.prefix(while: { $0 > 0 }).map(UInt8.init), encoding: String.Encoding.ascii)!
-        fatalError("\(reason), error message: \(errorMessage) (code: \(SDL_GetError()!.pointee))", line: line)
+        return "\(prefix), error message: \(errorMessage) (code: \(SDL_GetError()!.pointee))"
+}
+
+private func printSDLError(prefix: String) {
+    let errorMessage = sdlError(prefix: prefix)
+    print(errorMessage)
+}
+
+private func sdlFatalError(reason: String, _ line: UInt = #line) -> Never {
+    let errorMessage = sdlError(prefix: reason)
+    fatalError(errorMessage, line: line)
+}
+
+func textureFromImage(at filePath: String, renderer: OpaquePointer) -> OpaquePointer? {
+    guard let imageData = FileManager.default.contents(atPath: filePath) else {
+        printSDLError(prefix: "Failed to load image data at: \(filePath)")
+        return nil
+    }
+    let imageDataSize = Int32(imageData.count)
+    let sdlDataSource: UnsafeMutablePointer<SDL_RWops> = imageData.withUnsafeBytes {
+        guard
+            let imageDataAddress = $0.baseAddress,
+            let rwFromMem = SDL_RWFromConstMem(imageDataAddress, imageDataSize)
+        else {
+            sdlFatalError(reason: "PNG SDL_RWFromConstMem failed")
+        }
+        return rwFromMem
+    }
+    guard
+        let imageSurface = IMG_Load_RW(sdlDataSource, 1 /* `1` indicates that the stream will be closed after read */)
+        else {
+        printSDLError(prefix: "SDL_LoadFile_RW failed")
+        return nil
     }
     
+    defer { SDL_FreeSurface(imageSurface) }
+    
+    /* Upload surface to renderer, and then, free the surface */
+    guard let texture = SDL_CreateTextureFromSurface(renderer, imageSurface) else {
+        printSDLError(prefix: "SDL_CreateTextureFromSurface failed")
+        return nil
+    }
+    return texture
+}
+
+func testSDL() {
+    /* Starting SDL */
     guard SDL_Init(SDL_INIT_VIDEO) == 0 else  {
-        panic(reason: "SDL_INIT_VIDEO failed")
+        sdlFatalError(reason: "SDL_INIT_VIDEO failed")
     }
     
     /* Create a Window */
     guard let window = SDL_CreateWindow("Hello World", 0, 0, 640, 480, SDL_WINDOW_SHOWN.rawValue) else {
-        panic(reason: "Create Window failed")
+        sdlFatalError(reason: "Create Window failed")
     }
     
     /* Create a renderer */
     guard let renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED.rawValue | SDL_RENDERER_PRESENTVSYNC.rawValue) else {
-        panic(reason: "Create Renderer failed")
+        sdlFatalError(reason: "Create Renderer failed")
     }
-    
-//    guard let pngSurface: UnsafeMutablePointer<SDL_Surface> = IMG_Load(pngImagePath) else {
-//        panic(reason: "`IMG_Load` failed")
-//    }
-//    print("Successfully loaded pngSurface 🚀, height: \(pngSurface.pointee.h), width: \(pngSurface.pointee.w)")
-    
-    guard let pngData = FileManager.default.contents(atPath: "/Users/sajjon/Developer/Fun/Games/HoMM/SuccessionsKrigen/Sources/SuccessionsKrigen/homm2_battle.png") else {
-        panic(reason: "Failed to load png image as data using Swift FileManager")
-    }
-    let rwPng: UnsafeMutablePointer<SDL_RWops> = pngData.withUnsafeBytes {
-        guard let rwFromMem = SDL_RWFromConstMem($0.baseAddress!, Int32(pngData.count)) else {
-            panic(reason: "PNG SDL_RWFromConstMem failed")
-        }
-        return rwFromMem
-    }
-    print("✅ rwPng")
-    guard
-        let pngImageSurfaceFromRW = IMG_Load_RW(rwPng, 1 /* `1` indicates that the stream will be closed after read */)
-        else {
-        panic(reason: "SDL_LoadFile_RW failed")
-    }
-    print("🙌 Successfully SDL_LoadFile_RW and size")
-    
-//    /* Load bitmap image */
-//    let rw: UnsafeMutablePointer<SDL_RWops> = hommSpellBMP.withUnsafeBytes {
-//        guard let rwFromMem = SDL_RWFromConstMem($0.baseAddress!, Int32(hommSpellBMP.count)) else {
-//            panic(reason: "SDL_RWFromConstMem failed")
-//        }
-//        return rwFromMem
-//    }
-//    guard let imageSurface = SDL_LoadBMP_RW(rw, 1 /* `1` indicates that the stream will be closed after read */) else {
-//        panic(reason: "SDL_LoadBMP_RW failed")
-//    }
-    
-    
-    /* Upload surface to renderer, and then, free the surface */
-    guard let texture = SDL_CreateTextureFromSurface(renderer, pngImageSurfaceFromRW) else {
-        panic(reason: "SDL_CreateTextureFromSurface failed")
-    }
-    SDL_FreeSurface(pngImageSurfaceFromRW)
-    
+
     /* Draw the renderer on window */
     SDL_RenderClear(renderer) // Fill renderer with color
+    
+    guard
+        let texture = textureFromImage(
+            at: "/Users/sajjon/Developer/Fun/Games/HoMM/SuccessionsKrigen/Sources/SuccessionsKrigen/homm2_battle.png",
+            renderer: renderer
+    ) else {
+        fatalError("Failed to create image texture from image.")
+    }
+    
     SDL_RenderCopy(renderer, texture, nil, nil) // Copy the texture into renderer
     SDL_RenderPresent(renderer) // Show renderer on window
     
