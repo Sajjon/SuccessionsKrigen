c8a7c8d72ede1c681ce32c21e73852907120299c
Alexander Cyon <alex.cyon@gmail.com>
2021-08-08 19:46:46 +0200

    Add missing 'products' config in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,12 @@ import PackageDescription
 let package = Package(
     name: "SuccessionsKrigen",
     platforms: [.macOS(.v11)],
+    products: [
+         // Products define the executables and libraries a package produces, and make them visible to other packages.
+         .library(
+             name: "SuccessionsKrigen",
+             targets: ["SuccessionsKrigen"]),
+     ],
     dependencies: [
         .package(name: "SwiftSDL", url: "https://github.com/KevinVitale/SwiftSDL", .branch("master"))
 //        .package(name: "SDL2", url: "https://github.com/ctreffs/SwiftSDL2.git", from: "1.2.0")
